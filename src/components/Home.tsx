d4d44bdb2a83b277f6fb520db3ad4dbfbede820f
OdeliaFink <odelia.finkel@gmail.com>
2023-05-03 16:20:51 -0400

    Initial commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Gallery from './Gallery';
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <Gallery />
+    </div>
+  );
+};
+
+export default Home;
