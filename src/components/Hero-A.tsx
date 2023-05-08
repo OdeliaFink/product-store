37ca9fa50519e88431091189f079d43ef5ddef8b
OdeliaFink <odelia.finkel@gmail.com>
2023-05-08 17:07:47 -0400

    Refactor

--- src/components/Hero-A.tsx
+++ src/components/Hero-A.tsx
@@ -4,59 +4,24 @@ import 'react-multi-carousel/lib/styles.css';
 import styled from 'styled-components';
 
 const HeroCarouselA: React.FC = () => {
-  const texts = [
-    {
-      title: 'Discover our new collection',
-      description: 'One line text for image 1',
-    },
-    // { title: 'Title for image 2', description: 'One line text for image 2' },
-    // { title: 'Title for image 3', description: 'One line text for image 3' },
-  ];
-  const images = [
-    // image1,
-    'https://images.pexels.com/photos/1848662/pexels-photo-1848662.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    // 'https://images.pexels.com/photos/1848662/pexels-photo-1848662.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    // 'https://images.pexels.com/photos/1848662/pexels-photo-1848662.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-  ];
-
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 1,
-    },
-    mobile: {
-      breakpoint: { max: 1024, min: 0 },
-      items: 1,
-    },
-  };
-
   return (
     <div style={{ overflowX: 'hidden', overflowY: 'hidden' }}>
       <CarouselContainer>
-        {/* <Carousel
-          responsive={responsive}
-          infinite
-          autoPlay={false}
-          autoPlaySpeed={5000}
-          arrows
-          showDots={false}
-          renderButtonGroupOutside
-          // customButtonGroup={<ButtonGroup />}
-          containerClass="hero-carousel"
-        > */}
-
         <CarouselItem>
           <h2
             style={{
               position: 'absolute',
-              paddingBottom: '20rem',
-              fontSize: '3.5rem',
-              letterSpacing: '.4rem',
+
+              fontSize: '3rem',
+              color: 'white',
               width: '40%',
-              textAlign: 'left',
+              textAlign: 'center',
+              fontWeight: '400',
             }}
           >
-            TEXT
+            Sunday Best <br />
+            The Summer 23 <br />
+            Collection It's daydream season.
           </h2>
 
           <CarouselVideo autoPlay>
@@ -65,13 +30,7 @@ const HeroCarouselA: React.FC = () => {
               type="video/mp4"
             />
           </CarouselVideo>
-          <CallToAction>
-            {/* <p>{texts[index].description}</p> */}
-            <CallToActionButton>Call to Action</CallToActionButton>
-          </CallToAction>
         </CarouselItem>
-
-        {/* </Carousel> */}
       </CarouselContainer>
     </div>
   );
@@ -92,19 +51,6 @@ const CarouselItem = styled.div`
   flex-direction: column;
   align-items: center;
   text-align: center;
-
-  &:before {
-    content: '';
-    position: absolute;
-    top: 80%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 40%;
-    height: 25%;
-    background-color: white;
-    opacity: 0.7;
-    border-radius: 2%;
-  }
 `;
 
 const CarouselVideo = styled.video.attrs({
@@ -113,7 +59,7 @@ const CarouselVideo = styled.video.attrs({
   loop: true,
 })`
   width: 100%;
-  height: 85vh;
+  height: 90vh;
   object-fit: cover;
   z-index: -1;
   opacity: 0.8;
