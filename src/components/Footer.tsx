cff53ffb6e16a47e8e804dcabd39f9a27ed766c3
OdeliaFink <odelia.finkel@gmail.com>
2023-05-14 21:13:00 -0400

    refactor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,6 +9,9 @@ const Container = styled.footer`
 
   bottom: 0;
   width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
 
 const Text = styled.p`
@@ -30,10 +33,42 @@ type FooterProps = {
 const Footer: React.FC<FooterProps> = ({ companyName, year }) => {
   return (
     <Container>
-      <Text>
-        &copy; {year} {companyName}. All rights reserved. | Designed by{' '}
-        <Link href="https://yourdesigner.com">Aritzia</Link>
-      </Text>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div style={{ textAlign: 'left' }}>
+          <h6 style={{ fontSize: '1rem', fontWeight: '300', color: 'grey' }}>
+            JOIN ARITZIA'S MAILING LIST
+          </h6>
+          <h6 style={{ fontSize: '1', fontWeight: '200' }}>
+            Insider info on sales, new arrivals and more good stuff.
+          </h6>
+          <input
+            type="text"
+            id="fname"
+            name="firstname"
+            placeholder="Your name.."
+          />
+        </div>
+        <div style={{ textAlign: 'left' }}>
+          <h6 style={{ fontSize: '1rem', fontWeight: '300', color: 'grey' }}>
+            JOIN ARITZIA'S MAILING LIST
+          </h6>
+          <h6 style={{ fontSize: '1', fontWeight: '200' }}>
+            Insider info on sales, new arrivals and more good stuff.
+          </h6>
+          <input
+            type="text"
+            id="fname"
+            name="firstname"
+            placeholder="Your name.."
+          />
+        </div>
+      </div>
+      <div>
+        <Text>
+          &copy; {year} {companyName}. All rights reserved. | Designed by{' '}
+          <Link href="https://yourdesigner.com">Aritzia</Link>
+        </Text>
+      </div>
     </Container>
   );
 };
